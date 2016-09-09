f15e88bf3c151010800b97ab5a9df6cfaf8c2c1a
U-nity <728501875@qq.com>
2016-09-09 10:22:26 +0800

    Initial commit

---
+++ app/src/main/java/bistu/rookie/u_nity/androidproject_15/MainActivity.java
@@ -0,0 +1,30 @@
+package bistu.rookie.u_nity.androidproject_15;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
+import android.net.Uri;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button=(Button)findViewById(R.id.DialButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setAction(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:1234567890"));
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
