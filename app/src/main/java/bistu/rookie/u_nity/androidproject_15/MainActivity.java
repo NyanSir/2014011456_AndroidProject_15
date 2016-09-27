c74de3fceb2ce8ab865f52de8fdb47d521acabc7
U-nity <728501875@qq.com>
2016-09-27 14:26:57 +0800

    Some Revise

--- app/src/main/java/bistu/rookie/u_nity/androidproject_15/MainActivity.java
+++ app/src/main/java/bistu/rookie/u_nity/androidproject_15/MainActivity.java
@@ -14,11 +14,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button=(Button)findViewById(R.id.DialButton);
+        Button button = (Button)findViewById(R.id.DialButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent();
+                Intent intent = new Intent();
                 intent.setAction(Intent.ACTION_DIAL);
                 intent.setData(Uri.parse("tel:1234567890"));
                 startActivity(intent);
